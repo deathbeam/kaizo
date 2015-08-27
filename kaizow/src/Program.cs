5d7811d7987dfa649c5d7bdb926a5bcbda8ce3eb
Tomas Slusny <slusnucky@gmail.com>
2015-08-27 22:42:59 +0200

    Added some debug messages when download Kaizo with kaizow

--- kaizow/src/Program.cs
+++ kaizow/src/Program.cs
@@ -11,25 +11,39 @@ namespace Kaizow
 	class MainClass
 	{
 		private static readonly string HOME = Path.Combine(Environment.GetFolderPath (Environment.SpecialFolder.UserProfile), ".kaizo");
-		private static readonly string URL = "https://github.com/nondev/kaizo/archive/master.zip";
+		private static readonly string URL = "http://github.com/nondev/kaizo/archive/master.zip";
 		private static readonly string ZIP = Path.Combine(HOME, "kaizo.zip");
 		private static readonly string BOOT = Path.Combine(HOME, "kaizo-master", "bootstrap");
 
 		public static void Main (string[] args)
 		{
-			if (!Directory.Exists (BOOT) || (args.Length > 0 && args[0] == "update"))
+			bool doUpdate = args.Length > 0 && args[0] == "update";
+
+			if (!Directory.Exists (BOOT) || (doUpdate))
 			{
+				Console.Write("Downloading " + URL);
 				Directory.CreateDirectory (HOME);
 
 				using (var client = new WebClient ()) {
 					client.DownloadFile (URL, ZIP);
 				}
 
+				Console.ForegroundColor = ConsoleColor.Magenta;
+				Console.WriteLine(" [DONE]");
+				Console.ResetColor();
+
+				Console.Write("Extracting " + ZIP);
 				using (var unzip = new Unzip (ZIP)) {
 					unzip.ExtractToDirectory (HOME);
 				}
 
 				File.Delete (ZIP);
+
+				Console.ForegroundColor = ConsoleColor.Magenta;
+				Console.WriteLine(" [DONE]");
+				Console.ResetColor();
+
+				if (doUpdate) return;
 			}
 
 			Assembly.Load(File.ReadAllBytes(Path.Combine(BOOT, "KopiLua.dll")));
