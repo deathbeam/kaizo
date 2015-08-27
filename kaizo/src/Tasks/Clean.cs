0554eb033477dfe45e4de9787e065ead5321c70a
Tomas Slusny <slusnucky@gmail.com>
2015-08-27 23:40:33 +0200

    Fixed clean task (now really)

--- kaizo/src/Tasks/Clean.cs
+++ kaizo/src/Tasks/Clean.cs
@@ -3,19 +3,19 @@ using NLua;
 
 namespace Kaizo.Tasks
 {
-	public class Clean : Task
-	{
-		public Clean(Lua lua) : base(lua) { }
+  public class Clean : Task
+  {
+    public Clean(Lua lua) : base(lua) { }
 
-		public void Run(string project) {
+    public void Run(string project) {
       var output = lua [project + ".csharp.output"] as string;
-			if (output == null) output = "out";
+      if (output == null) output = "out";
 
-			var resources = lua [project + ".csharp.resources"] as string;
-			if (resources == null) resources = "res";
+      var resources = lua [project + ".csharp.resources"] as string;
+      if (resources == null) resources = "res";
 
-			if (Directory.Exists(output)) Directory.Delete(output, true);
-			if (Directory.Exists(resources)) Directory.Delete(resources, true);
+      if (Directory.Exists(output)) Directory.Delete(output, true);
+      if (Directory.Exists(resources)) Directory.Delete(resources, true);
     }
-	}
+  }
 }
