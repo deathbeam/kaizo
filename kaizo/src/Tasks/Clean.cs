6afdd3f7183b49263bb3a5f675f8258f8c35c8da
Tomas Slusny <slusnucky@gmail.com>
2015-08-28 00:24:50 +0200

    Clean task was removing resources folder by accident

--- kaizo/src/Tasks/Clean.cs
+++ kaizo/src/Tasks/Clean.cs
@@ -11,11 +11,7 @@ namespace Kaizo.Tasks
       var output = lua [project + ".csharp.output"] as string;
       if (output == null) output = "out";
 
-      var resources = lua [project + ".csharp.resources"] as string;
-      if (resources == null) resources = "res";
-
       if (Directory.Exists(output)) Directory.Delete(output, true);
-      if (Directory.Exists(resources)) Directory.Delete(resources, true);
     }
   }
 }
