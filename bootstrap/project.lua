75a88f4e164b05bef1f81dd06c36b595e2ccd701
Tomas Slusny <slusnucky@gmail.com>
2015-09-08 21:09:22 +0200

    Space and tabs

--- bootstrap/project.lua
+++ bootstrap/project.lua
@@ -5,28 +5,28 @@ version = '0.0.1'
 source = '../kaizo/src'
 
 configuration = {
-	outputType = 'exe',
-	outputPath = 'bin',
-	rootNamespace = 'Kaizo',
-	platformTarget = 'anycpu',
-	targetFrameworkVersion = 'v4.0'
+  outputType = 'exe',
+  outputPath = 'bin',
+  rootNamespace = 'Kaizo',
+  platformTarget = 'anycpu',
+  targetFrameworkVersion = 'v4.0'
 }
 
 dependencies = {
-	system = {
-		'System',
-		'System.Core',
-		'Microsoft.Build',
-		'Microsoft.Build.Framework'
-	},
-	nuget = {
-		'NLua_Safe',
-		'Mono.NuGet.Core',
-		'Microsoft.Web.Xdt'
-	}
+  system = {
+    'System',
+    'System.Core',
+    'Microsoft.Build',
+    'Microsoft.Build.Framework'
+  },
+  nuget = {
+    'NLua_Safe',
+    'Mono.NuGet.Core',
+    'Microsoft.Web.Xdt'
+  }
 }
 
 function compile()
-	task('self.clean')
-	task('self.build')
+  task('self.clean')
+  task('self.build')
 end
